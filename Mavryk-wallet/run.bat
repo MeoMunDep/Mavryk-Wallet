82c305ad4efc831858ac033155180388307d3add
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-04-06 13:23:42 +0700

    Update run.bat

--- Mavryk-wallet/run.bat
+++ Mavryk-wallet/run.bat
@@ -12,6 +12,7 @@ if not exist configs.json (
     echo   "restartInterval": 10,>> configs.json
     echo   "maxConcurrentWallets": 200,>> configs.json
     echo   "staking": false,>> configs.json
+    echo   "createVault": true,>> configs.json
     echo   "depositToken": true,>> configs.json
     echo   "borrowToken": true,>> configs.json
     echo   "sendToken": {>> configs.json
